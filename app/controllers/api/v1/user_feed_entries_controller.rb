372ad4661019b61ce287b188f280b12c3eeca6d6
Magnus Skog <magnus.m.skog@gmail.com>
2016-03-25 11:22:49 +0100

    Pagination in params

--- app/controllers/api/v1/user_feed_entries_controller.rb
+++ app/controllers/api/v1/user_feed_entries_controller.rb
@@ -1,11 +1,15 @@
 class Api::V1::UserFeedEntriesController < Api::ApiController
   def index
     user_feed = UserFeed.eager_load(feed: :entries).find(params[:id])
-    entries = FeedEntryDecorator.decorate_collection(user_feed.feed.entries.order(id: :desc).page(1).per(20))
+    entries = FeedEntryDecorator.decorate_collection(user_feed.feed.entries.order(id: :desc).page(page).per(20))
     render json: entries, each_serializer: FeedEntrySerializer, meta: meta_attributes(entries)
   end
 
   private
+
+  def page
+    params[:page] || 1
+  end
 
   def meta_attributes(resource, extra_meta = {})
     {
