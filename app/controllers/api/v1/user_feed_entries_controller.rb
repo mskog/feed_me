1d4c98f33b198aa93ebdc29f3289c2c1e298b648
Magnus Skog <magnus.m.skog@gmail.com>
2016-04-03 12:20:48 +0200

    Faster

--- app/controllers/api/v1/user_feed_entries_controller.rb
+++ app/controllers/api/v1/user_feed_entries_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UserFeedEntriesController < Api::ApiController
   def index
-    user_feed = UserFeed.eager_load(feed: :entries).find(params[:id])
+    user_feed = UserFeed.includes(feed: :entries).find(params[:id])
     entries = FeedEntryDecorator.decorate_collection(user_feed.feed.entries.order(id: :desc).page(page).per(20))
     render json: entries, each_serializer: FeedEntrySerializer, meta: meta_attributes(entries)
   end
